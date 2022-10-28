6f779c438dc702b64c64f8dbbbc985a516f68ca6
Siqi Song <ssong6@slb.com>
2022-10-28 16:56:12 +0800

    feat: return 4 random different digit number when generate secret

--- BullsAndCows/SecretGenerator.cs
+++ BullsAndCows/SecretGenerator.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BullsAndCows
 {
     public class SecretGenerator
     {
+        private const int SecreteLength = 4;
+        private Random random;
+        public SecretGenerator()
+        {
+            random = new Random();
+        }
+
+        public SecretGenerator(Random random)
+        {
+            this.random = random;
+        }
+
         public virtual string GenerateSecret()
         {
-            return "1 1 1 1";
+            var secretList = new List<int>();
+            for (int length = 0; length < 4;)
+            {
+                int secretNumber = random.Next(10);
+                if (!secretList.Contains(secretNumber))
+                {
+                    secretList.Add(secretNumber);
+                    length++;
+                }
+            }
+
+            return string.Join(" ", secretList);
         }
     }
 }
--- BullsAndCowsTest/SecretGeneratorTest.cs
+++ BullsAndCowsTest/SecretGeneratorTest.cs
@@ -1,4 +1,5 @@
 ﻿using BullsAndCows;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,20 @@ namespace BullsAndCowsTest
             string secret = secretGenerator.GenerateSecret();
             //then
             Assert.Equal(4, secret.Split(" ").Length);
+        }
+
+        [Fact]
+        public void Should_return_4_random_different_digit_number_when_generat_secret()
+        {
+            //given
+            var mockRandom = new Mock<Random>();
+            mockRandom.SetupSequence(random => random.Next(It.IsAny<int>()))
+                .Returns(1).Returns(1).Returns(5).Returns(3).Returns(5).Returns(4).Returns(9).Returns(1);
+            var secretGenerator = new SecretGenerator(mockRandom.Object);
+            //when
+            string secret = secretGenerator.GenerateSecret();
+            //then
+            Assert.Equal("1 5 3 4", secret);
         }
     }
 }
