28360b151ee9ce2d7cc4115e953907191c48b59c
Siqi Song <ssong6@slb.com>
2022-10-28 16:04:56 +0800

    feat: return 4 digit number when generate secret

---
+++ BullsAndCowsTest/SecretGeneratorTest.cs
@@ -0,0 +1,24 @@
+﻿using BullsAndCows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BullsAndCowsTest
+{
+    public class SecretGeneratorTest
+    {
+        [Fact]
+        public void Should_return_4_digit_number_when_generat_secret()
+        {
+            //given
+            var secretGenerator = new SecretGenerator();
+            //when
+            string secret = secretGenerator.GenerateSecret();
+            //then
+            Assert.Equal(4, secret.Split(" ").Length);
+        }
+    }
+}
