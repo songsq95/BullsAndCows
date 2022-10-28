21a8e081dc6d839e6c4e1e602a4aae6bf14edc0d
Siqi Song <ssong6@slb.com>
2022-10-28 14:05:34 +0800

    feat: return 4A0B when guess 4 digits

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,4 +1,5 @@
 using BullsAndCows;
+using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -12,6 +13,21 @@ namespace BullsAndCowsTest
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_return_4A0B_when_guess_given_digits_are_the_same_as_secrete()
+        {
+            //given
+            var stubSecretGenerator=new Mock<SecretGenerator>();
+            stubSecretGenerator.Setup(generator => generator.GenerateSecret())
+                .Returns("1 2 3 4");
+            var game = new BullsAndCowsGame(stubSecretGenerator.Object);
+            //when
+            var guessResult = game.Guess("1 2 3 4");
+
+            //then
+            Assert.Equal("4A0B", guessResult);
         }
     }
 }
